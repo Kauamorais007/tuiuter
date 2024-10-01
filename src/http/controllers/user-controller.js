3a5bc44715a7bd0bb3debd8dca1d09b89288c7f5
Kauã Morais <kaua.morais22@escola.pr.gov.br>
2024-10-01 11:40:01 -0300

    OK K

--- src/http/controllers/user-controller.js
+++ src/http/controllers/user-controller.js
@@ -1,33 +1,12 @@
-import User from "../../../database/models/user-model.js";
+import User from "../../database/models/user-model.js";
 import jwtService from "../services/jwt-service.js";
-import bcrypt from 'bcrypt';
-
-export const signup = async (req, res) => {
-  try {
-    // Set nickname to email if not provided
-    const nickname = req.body.nickname || req.body.email;
-
-    // Hash the password before saving
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-    const user = await User.create({
-      email: req.body.email,
-      password: hashedPassword,
-      nickname,
-    });
-
-    const token = jwtService.generateAccessToken(user);
-    res.status(201).json({ token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+import bcrypt from "bcrypt";
 
 export const login = async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
 
-    if (user && await bcrypt.compare(req.body.password, user.password)) {
+    if (user && (await bcrypt.compare(req.body.password, user.password))) {
       const token = jwtService.generateAccessToken(user);
       res.json({ token });
     } else {
@@ -38,3 +17,18 @@ export const login = async (req, res) => {
   }
 };
 
+export const signup = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    await User.create({
+      email,
+      password,
+    });
+
+    const token = generateToken(user);
+
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
